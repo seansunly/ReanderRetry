e0b6d47efd332253ba5c1f060a3b459e9d3ca141
seansunly <sunlyslysun@gmail.com>
2024-06-14 20:43:41 +0700

    readerRetry

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+import { CardComponent } from "../coponets/CardCoponets.js";
+import { product } from "../data/product.js";
+//import { product } from "../data/product.js";
+
+let reamdome = document.querySelector("#renderArea");
+// //product.map((product) => (reamdome.innerHTML += CardComponent(product)));
+// //const Based_url = "http://127.0.0.1:5500/Render-product/data/product.json";
+// fetch(Based_url)
+//     .then((res)=>res.json())
+//     .then((datas)=>{
+//         let products = datas.product;
+//         products.map((products2)=>(reamdome.innerHTML+=CardComponent(products2)));
+//     })
+product.map((producta)=>reamdome.innerHTML+=CardComponent(producta));
+
+
+
+    
